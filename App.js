09c4696d5a1802502c0d99a1baafc451dd1799fa
Luthfinizar <48509231+Luthfinizar@users.noreply.github.com>
2019-10-16 16:00:31 +0700

    Add files via upload
    Sorry it's still messy, may be usefull

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainProg from './src/Navigation/Navigasi';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <MainProg/>
+    );
+  }
+}
